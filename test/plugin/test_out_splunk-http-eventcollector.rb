23355d12e01c3aa345ab426ee6b056c86b04c7f1
Bryce Chidester <bryce@cobryce.com>
2018-03-01 12:24:19 -0800

    More futzing with tests
    Tweaking tests to they each run twice (integer and float forms)
    properly, hopefully. Sorry for the noise.

--- test/plugin/test_out_splunk-http-eventcollector.rb
+++ test/plugin/test_out_splunk-http-eventcollector.rb
@@ -24,12 +24,12 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
   end
 
   def test_write
-    stub_request(:post, "https://localhost:8089/services/collector/event").
-      to_return(body: '{"text":"Success","code":0}')
-
-    d = create_driver
-
     [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      stub_request(:post, "https://localhost:8089/services/collector/event").
+        to_return(body: '{"text":"Success","code":0}')
+
+      d = create_driver
+
       d.emit({ "message" => "a message"}, time)
 
       d.run
@@ -40,38 +40,39 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
           'Content-Type' => 'application/json',
           'User-Agent' => 'fluent-plugin-splunk-http-eventcollector/0.0.1'
         },
-        body: { time: time, source:"test", sourcetype: "fluentd", host: "", index: "main", event: "a message" }
+        body: { time: time, source:"test", sourcetype: "fluentd", host: "", index: "main", event: "a message" },
+        times: (case time when Integer then 1 when Float then 2 end)
     end
   end
 
   def test_expand
-    stub_request(:post, "https://localhost:8089/services/collector/event").
-      to_return(body: '{"text":"Success","code":0}')
-
-    d = create_driver(CONFIG + %[
-      source ${record["source"]}
-      sourcetype ${tag_parts[0]}
-    ])
-
     [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      stub_request(:post, "https://localhost:8089/services/collector/event").
+        to_return(body: '{"text":"Success","code":0}')
+
+      d = create_driver(CONFIG + %[
+        source ${record["source"]}
+        sourcetype ${tag_parts[0]}
+      ])
+
       d.emit({"message" => "a message", "source" => "source-from-record"}, time)
 
       d.run
 
       assert_requested :post, "https://localhost:8089/services/collector/event",
         headers: {"Authorization" => "Splunk changeme"},
-        body: { time: time, source: "source-from-record", sourcetype: "test", host: "", index: "main", event: "a message" }
+        body: { time: time, source: "source-from-record", sourcetype: "test", host: "", index: "main", event: "a message" },
+        times: (case time when Integer then 1 when Float then 2 end)
     end
   end
 
   def test_4XX_error_retry
-    stub_request(:post, "https://localhost:8089/services/collector/event").
-      with(headers: {"Authorization" => "Splunk changeme"}).
-      to_return(body: '{"text":"Incorrect data format","code":5,"invalid-event-number":0}', status: 400)
-
-    d = create_driver
-
     [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      stub_request(:post, "https://localhost:8089/services/collector/event").
+        with(headers: {"Authorization" => "Splunk changeme"}).
+        to_return(body: '{"text":"Incorrect data format","code":5,"invalid-event-number":0}', status: 400)
+
+      d = create_driver
       d.emit({ "message" => "1" }, time)
       d.run
 
@@ -83,11 +84,12 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
   end
 
   def test_5XX_error_retry
-    request_count = 0
-    stub_request(:post, "https://localhost:8089/services/collector/event").
-      with(headers: {"Authorization" => "Splunk changeme"}).
-      to_return do |request|
-        request_count += 1
+    [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      request_count = 0
+      stub_request(:post, "https://localhost:8089/services/collector/event").
+        with(headers: {"Authorization" => "Splunk changeme"}).
+        to_return do |request|
+          request_count += 1
 
         if request_count < 5
           { body: '{"text":"Internal server error","code":8}', status: 500 }
@@ -96,14 +98,10 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
         end
       end
 
-
-    d = create_driver(CONFIG + %[
-      post_retry_max 5
-      post_retry_interval 0.1
-    ])
-
-    [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
-      request_count = 0
+      d = create_driver(CONFIG + %[
+        post_retry_max 5
+        post_retry_interval 0.1
+      ])
       d.emit({ "message" => "1" }, time)
       d.run
 
@@ -115,16 +113,15 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
   end
 
   def test_write_splitting
+    [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
     stub_request(:post, "https://localhost:8089/services/collector/event").
       with(headers: {"Authorization" => "Splunk changeme"}).
       to_return(body: '{"text":"Incorrect data format","code":5,"invalid-event-number":0}', status: 400)
 
-    # A single msg is ~110 bytes
-    d = create_driver(CONFIG + %[
-      batch_size_limit 250
-    ])
-
-    [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      # A single msg is ~110 bytes
+      d = create_driver(CONFIG + %[
+        batch_size_limit 250
+      ])
       d.emit({"message" => "a" }, time)
       d.emit({"message" => "b" }, time)
       d.emit({"message" => "c" }, time)
@@ -138,26 +135,26 @@ class SplunkHTTPEventcollectorOutputTest < Test::Unit::TestCase
       assert_requested :post, "https://localhost:8089/services/collector/event",
         headers: {"Authorization" => "Splunk changeme"},
         body: { time: time, source: "test", sourcetype: "fluentd", host: "", index: "main", event: "c" }.to_json
+      assert_requested :post, "https://localhost:8089/services/collector/event", times: 2
     end
-    assert_requested :post, "https://localhost:8089/services/collector/event", times: 4
   end
 
   def test_utf8
-    stub_request(:post, "https://localhost:8089/services/collector/event").
-      with(headers: {"Authorization" => "Splunk changeme"}).
-      to_return(body: '{"text":"Success","code":0}')
-
-    d = create_driver(CONFIG + %[
-      all_items true
-    ])
-
     [TIME_OBJ.to_i, TIME_OBJ.to_f].each do |time|
+      stub_request(:post, "https://localhost:8089/services/collector/event").
+        with(headers: {"Authorization" => "Splunk changeme"}).
+        to_return(body: '{"text":"Success","code":0}')
+
+      d = create_driver(CONFIG + %[
+        all_items true
+      ])
       d.emit({ "some" => { "nested" => "ü†f-8".force_encoding("BINARY"), "with" => ['ü', '†', 'f-8'].map {|c| c.force_encoding("BINARY") } } }, time)
       d.run
 
       assert_requested :post, "https://localhost:8089/services/collector/event",
         headers: {"Authorization" => "Splunk changeme"},
-        body: { time: time, source: "test", sourcetype: "fluentd", host: "", index: "main", event: { some: { nested: "     f-8", with: ["  ","   ","f-8"]}}}
+        body: { time: time, source: "test", sourcetype: "fluentd", host: "", index: "main", event: { some: { nested: "     f-8", with: ["  ","   ","f-8"]}}},
+        times: (case time when Integer then 1 when Float then 2 end)
     end
   end
 end
